6282aa601eb43a5f11f6b3c933ee4b82282073a8
Karan Jeswani <krnjeswani21@gmail.com>
2023-05-14 22:06:43 +0530

    first commit with cra and Menu

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Menu from "./Menu"
+
+function App() {
+  return (
+    <>
+    <Menu />
+    <div className="text-5xl font-bold">Hello World</div>
+    </>
+  );
+}
+
+export default App;
