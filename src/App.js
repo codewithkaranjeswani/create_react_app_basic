e52b1b098d50d5b0b7d77cb77dac1a81016b93b6
Karan Jeswani <krnjeswani21@gmail.com>
2023-06-11 22:25:38 +0530

    trying carousel component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react"
-import Menu from "./Menu"
+import React from "react";
+import Menu from "./Menu";
+import Carousel from "./components/Carousel";
 
 function App() {
+  const slides = [
+    "./images/2.png",
+    "./images/1c.png",
+    "./images/3.png",
+    "./images/4.png",
+  ];
+
   return (
     <>
-    <Menu />
-    <div className="text-5xl font-bold">Hello World</div>
+      <Menu />
+      <div className="flex justify-center text-5xl font-bold">Hello World</div>
+      <div className="max-w-lg mx-auto">
+        <Carousel>
+          {slides.map((s) => (
+            <img src={s} />
+          ))}
+        </Carousel>
+      </div>
     </>
   );
 }
